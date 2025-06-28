ad8e8f8a4b55417e0b19c2620a413d2df5caa4d1
Hadi <969hadiahmed@gmail.com>
2025-06-28 18:35:30 +0530

    feat: prisma client setup

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.concurrency import asynccontextmanager
+from app.db.client import db
 from app.routers import auth
 
-app = FastAPI()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    await db.connect()
+    yield
+    # Shutdown
+    await db.disconnect()
+    
+app = FastAPI(lifespan=lifespan)
 app.include_router(auth.router)

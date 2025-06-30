c6ebf9da3c0ce2c86ee79422997d4a96feda2439
Hadi <969hadiahmed@gmail.com>
2025-06-30 12:43:47 +0530

    feat: services to crete and get plans

---
+++ app/services/plan_service.py
@@ -0,0 +1,17 @@
+from typing import List
+from app.db.client import db
+from app.schemas.plan_schema import PlanCreate
+from prisma.models import Plan
+
+
+async def create_plan(company_id,plan_data: PlanCreate)->Plan:
+    return await db.plan.create(data={
+        'companyId':company_id,
+        'name':plan_data.name,
+        'description':plan_data.description,
+        'cost':plan_data.cost,
+        'discount':plan_data.discount
+    })
+    
+async def find_plans()->List[Plan]:
+    return await db.plan.find_many()

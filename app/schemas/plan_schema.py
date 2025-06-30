037bdbcb2582c28680e70a395fc80877909fe206
Hadi <969hadiahmed@gmail.com>
2025-06-30 19:03:13 +0530

    feat: adding features with plan

--- app/schemas/plan_schema.py
+++ app/schemas/plan_schema.py
@@ -1,7 +1,9 @@
+from typing import List
 from pydantic import BaseModel, Field
 
 class PlanCreate(BaseModel):
     name: str
     description: str
     cost: float = Field(..., ge=0, description="Cost must be non-negative")
-    discount: int = Field(..., ge=0, le=100, description="Discount must be between 0 and 100")
+    discount: int = Field(0, ge=0, le=100, description="Discount must be between 0 and 100")
+    feature_ids: List[str]=[]
--- app/services/plan_service.py
+++ app/services/plan_service.py
@@ -1,19 +1,29 @@
 from typing import List
 
-from fastapi import HTTPException
+from fastapi import Depends, HTTPException
+from app.core.dependancy import get_current_company
 from app.db.client import db
 from app.schemas.plan_schema import PlanCreate
-from prisma.models import Plan
+from prisma.models import Plan,User
 
 
-async def create_plan(company_id,plan_data: PlanCreate)->Plan:
-    return await db.plan.create(data={
-        'companyId':company_id,
-        'name':plan_data.name,
-        'description':plan_data.description,
-        'cost':plan_data.cost,
-        'discount':plan_data.discount
-    })
+async def create_plan(company_id, plan_data: PlanCreate)->Plan:
+    feature_connections = [{"id": fid} for fid in plan_data.feature_ids]
+    return await db.plan.create(
+        data={
+            "companyId": company_id,
+            "name": plan_data.name,
+            "description": plan_data.description,
+            "cost": plan_data.cost,
+            "discount": plan_data.discount,
+            "features": {
+                "create": [{"feature": {"connect": {"id": f["id"]}}} for f in feature_connections]
+            }
+        },
+        include={
+            "features": True
+        }
+    )
     
 async def find_plans()->List[Plan]:
     return await db.plan.find_many()

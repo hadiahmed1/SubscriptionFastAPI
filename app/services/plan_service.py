16b9c635ace43b96033823bbbd8c49e2d9c9e0ff
Hadi <969hadiahmed@gmail.com>
2025-07-02 11:16:05 +0530

    fix: plan_service

--- app/services/plan_service.py
+++ app/services/plan_service.py
@@ -4,28 +4,27 @@ from fastapi import Depends, HTTPException
 from app.core.dependancy import get_current_company
 from app.db.client import db
 from app.schemas.plan_schema import PlanCreate
-from prisma.models import Plan,User
+from prisma.models import Plan, User
+
 
 async def validate_feature_ids_for_company(feature_ids: list[str], company_id: str):
     company_features = await db.feature.find_many(
-        where={"companyId": company_id},
-        select={"id": True}
+        where={"companyId": company_id}, select={"id": True}
     )
     valid_feature_ids = {f["id"] for f in company_features}
 
     invalid_ids = [fid for fid in feature_ids if fid not in valid_feature_ids]
     if invalid_ids:
         raise HTTPException(
-            status_code=400,
-            detail=f"Invalid feature IDs for company: {invalid_ids}"
+            status_code=400, detail=f"Invalid feature IDs for company: {invalid_ids}"
         )
 
 
-async def create_plan(company_id, plan_data: PlanCreate)->Plan:
+async def create_plan(company_id, plan_data: PlanCreate) -> Plan:
     feature_connections = [{"id": fid} for fid in plan_data.feature_ids]
-    
+
     await validate_feature_ids_for_company(plan_data.feature_ids, company_id)
-     
+
     return await db.plan.create(
         data={
             "companyId": company_id,
@@ -34,19 +33,31 @@ async def create_plan(company_id, plan_data: PlanCreate)->Plan:
             "cost": plan_data.cost,
             "discount": plan_data.discount,
             "features": {
-                "create": [{"feature": {"connect": {"id": f["id"]}}} for f in feature_connections]
-            }
+                "create": [
+                    {"feature": {"connect": {"id": f["id"]}}}
+                    for f in feature_connections
+                ]
+            },
         },
+        include={"features": True},
+    )
+
+async def find_plans() -> List[Plan]:
+    return await db.plan.find_many(
         include={
-            "features": True
+            "company": True,
+            "features": {"include": {"feature": True}},
         }
     )
-    
-async def find_plans()->List[Plan]:
-    return await db.plan.find_many()
 
-async def find_plan_byID(id)->Plan:
-    plan=await db.plan.find_unique(where={'id':id})
+async def find_plan_byID(id) -> Plan:
+    plan = await db.plan.find_unique(
+        where={"id": id},
+        include={
+            "company": True,
+            "features": {"include": {"feature": True}},
+        },
+    )
     if plan is None:
         raise HTTPException(status_code=404, detail="Invalid Plan ID")
     return plan

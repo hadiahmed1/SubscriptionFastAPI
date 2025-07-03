3136d85ceb106307f9ec6446db1ffdd77cab0d28
Hadi <969hadiahmed@gmail.com>
2025-07-03 12:39:18 +0530

    feat: added validit

--- app/schemas/plan_schema.py
+++ app/schemas/plan_schema.py
@@ -6,4 +6,5 @@ class PlanCreate(BaseModel):
     description: str
     cost: float = Field(..., ge=0, description="Cost must be non-negative")
     discount: int = Field(0, ge=0, le=100, description="Discount must be between 0 and 100")
+    validity: int= Field(30, ge=1, description="Validity must be greater than 0")
     feature_ids: List[str]=[]

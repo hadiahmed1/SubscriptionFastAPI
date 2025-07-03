badcd4ca3779067d09aea5edaaa8e767340b3af8
Hadi <969hadiahmed@gmail.com>
2025-07-03 13:17:03 +0530

    fix: default token expiry after 40min

--- app/core/auth.py
+++ app/core/auth.py
@@ -5,6 +5,6 @@ from app.core.config import SECRET_KEY, ALGORITHM
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=40))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)

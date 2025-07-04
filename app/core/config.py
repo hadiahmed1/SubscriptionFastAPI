a88167dbab92f09a87f244917a6fbf184dc6cded
Hadi <969hadiahmed@gmail.com>
2025-07-04 11:05:32 +0530

    feat: importing secret from detenv

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,8 @@
-SECRET_KEY = "your-secret-key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))

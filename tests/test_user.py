e3c8e692006bf54edffe7beb2a9a2a2788717862
Hadi <969hadiahmed@gmail.com>
2025-07-08 15:04:48 +0530

    feat: Test to create new user

---
+++ tests/test_user.py
@@ -0,0 +1,23 @@
+import pytest
+from app.db.client import db
+
+# CREATE USER
+@pytest.mark.asyncio
+async def test_create_user(async_client):
+    # Creating User
+    user_create = {
+        "username": "string",
+        "email": "user@example.com",
+        "role": "USER",
+        "password": "string",
+    }
+    response = await async_client.post("/users/", json=user_create)
+    assert response.status_code == 201
+    user = response.json()
+    assert "id" in user
+    assert user["username"] == "string"
+    assert user["role"] == "USER"
+    assert user["email"] == "user@example.com"
+    assert user["password"] !="string"
+    # cleanup
+    await db.user.delete(where={"username":"string"})

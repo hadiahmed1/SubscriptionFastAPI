2304e2b905931afcb1c1a0f044af99b2288ee3fa
Hadi <969hadiahmed@gmail.com>
2025-07-08 12:53:13 +0530

    feat: Test For Wrong Password

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,8 @@
 import pytest
 
+# SUCCESSFUL LOGIN
 @pytest.mark.asyncio
-async def test_post_token(async_client):
+async def test_post_token_success(async_client):
     response = await async_client.post(
         "/auth/token", json={"username": "hadi", "password": "hadi"}
     )
@@ -10,3 +11,14 @@ async def test_post_token(async_client):
     assert data["message"] == "Login successful"
     assert data["user"]["username"] == "hadi"
     assert "access_token" in response.cookies
+
+# WRONG PASSWORD
+@pytest.mark.asyncio
+async def test_post_token_wrong_password(async_client):
+    response = await async_client.post(
+        "/auth/token", json={"username": "hadi", "password": "hadiahmed"}
+    )
+    assert response.status_code == 401
+    data = response.json()
+    assert data["detail"] == "Incorrect username or password"
+    

c0a7495f362f227959376bf623549e0157deb4b0
Hadi <969hadiahmed@gmail.com>
2025-07-08 12:53:55 +0530

    feat: Tesst for Invalid User name/email

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 # SUCCESSFUL LOGIN
 @pytest.mark.asyncio
 async def test_post_token_success(async_client):
@@ -12,6 +13,7 @@ async def test_post_token_success(async_client):
     assert data["user"]["username"] == "hadi"
     assert "access_token" in response.cookies
 
+
 # WRONG PASSWORD
 @pytest.mark.asyncio
 async def test_post_token_wrong_password(async_client):
@@ -21,4 +23,14 @@ async def test_post_token_wrong_password(async_client):
     assert response.status_code == 401
     data = response.json()
     assert data["detail"] == "Incorrect username or password"
-    
+
+
+# INVALID USER
+@pytest.mark.asyncio
+async def test_post_token_wrong_username(async_client):
+    response = await async_client.post(
+        "/auth/token", json={"username": "hadia", "password": "hadiahmed"}
+    )
+    assert response.status_code == 404
+    data = response.json()
+    assert data["detail"] == "User not found"

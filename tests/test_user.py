dd076c19bdc13946f217d918bba2eb4bfdd57752
Hadi <969hadiahmed@gmail.com>
2025-07-08 15:08:33 +0530

    feat: test to create duplicate user

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,6 +1,7 @@
 import pytest
 from app.db.client import db
 
+
 # CREATE USER
 @pytest.mark.asyncio
 async def test_create_user(async_client):
@@ -18,6 +19,22 @@ async def test_create_user(async_client):
     assert user["username"] == "string"
     assert user["role"] == "USER"
     assert user["email"] == "user@example.com"
-    assert user["password"] !="string"
+    assert user["password"] != "string"
     # cleanup
-    await db.user.delete(where={"username":"string"})
+    await db.user.delete(where={"username": "string"})
+
+
+# CREATE DUPLICATE USER
+@pytest.mark.asyncio
+async def test_create_duplicate_user(async_client):
+    # Creating Duplicate User
+    user_create = {
+        "username": "hadi", #duplicate field
+        "email": "user@example.com",
+        "role": "USER",
+        "password": "string",
+    }
+    response = await async_client.post("/users/", json=user_create)
+    assert response.status_code == 409
+    data = response.json()
+    assert data["detail"] == "User with this email or username already exists."
